d5fe20826114eae69ebd13382d914a951a2e12b3
nirmiter <nirmiter@iastate.edu>
2018-09-22 13:27:26 -0500

    Worked on recieving JSON files and parsing them

--- Experiments/NirmiterExperiments/Login/app/src/main/java/com/example/nirmiter/loginactivity/RegActivity.java
+++ Experiments/NirmiterExperiments/Login/app/src/main/java/com/example/nirmiter/loginactivity/RegActivity.java
@@ -2,12 +2,29 @@ package com.example.nirmiter.loginactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegActivity extends AppCompatActivity {
+
+    Button click;
+   public static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+
+        click = (Button) findViewById(R.id.jsonBTN);
+        data = (TextView) findViewById(R.id.jsonTXT);
+
+        click.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view){
+                fetchData process = new fetchData();
+                process.execute();
+            }
+        });
     }
 }
