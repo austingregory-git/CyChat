ede8fd2febb98695acad81e8a5b9ffe78b48e9ac
monkeyark <danryw@outlook.com>
2018-09-25 07:13:51 -0500

    add sql

---
+++ Experiments/danry/database/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE user
+(
+email varchar(255),
+password varchar(32),
+id integer,
+name varchar(20),
+identity varchar(20),
+gender varchar(1),
+department varchar(20),
+create_time timestamp,
+PRIMARY KEY(id)
+)
