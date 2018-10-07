879204a34ab719d2e3f2f98e4cb3dd425db2d093
monkeyark <danryw@outlook.com>
2018-10-06 20:01:03 -0500

    homepage update

--- Experiments/danry/html/index.js
+++ Experiments/danry/html/index.js
@@ -1,6 +1,17 @@
 
+function index()
+{
+    displayTime();
+    enter();
+}
+
 function enter()
 {
     window.open("https://git.linux.iastate.edu/cs309/Fall2018/SD_1", "_self");
     return false;
+}
+
+function displayTime()
+{
+    document.getElementById("time").innerHTML = new Date();
 }
