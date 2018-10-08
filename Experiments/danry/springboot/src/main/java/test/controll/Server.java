cb21a40463f77af5c783c48c4a255c7e1f1463c8
monkeyark <danryw@outlook.com>
2018-10-08 07:08:12 -0500

    merge danry

---
+++ Experiments/danry/springboot/src/main/java/test/controll/Server.java
@@ -0,0 +1,41 @@
+package test.controll;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Server {
+	
+	@Autowired
+	private User_data data;
+	
+	
+	
+	
+	public List<UserInfor> show()
+	{
+		List<UserInfor> temp = new ArrayList<>();
+		data.findAll().forEach(temp::add);
+		return temp;
+		
+	}
+	
+	public Optional<UserInfor> Find(int username)
+	{
+		String temp = username +"";
+		
+		return data.findById(temp);
+	}
+	
+	public void addUser(UserInfor user)
+	{
+		data.save(user);
+	}
+	
+
+}
