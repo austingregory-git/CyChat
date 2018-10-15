3a432cade981e3a65c301201aa4409c7a4b8aeb4
agregory <agregory@iastate.edu>
2018-10-15 14:50:40 -0500

    UI flow fully functional. Menu now complete. Login requirements now added.

--- Frontend/UserInterface/UI/app/src/main/java/edu/iastate/linux/git/ui/LoginActivity.java
+++ Frontend/UserInterface/UI/app/src/main/java/edu/iastate/linux/git/ui/LoginActivity.java
@@ -10,14 +10,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
     Button b1, b2;
-    Spinner menu;
+    EditText uname, pword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +28,24 @@ public class LoginActivity extends AppCompatActivity {
 
         b1 = (Button) findViewById(R.id.loginButton);
         b2 = (Button) findViewById(R.id.registrationButton);
+        uname = (EditText) findViewById(R.id.username);
+        pword = (EditText) findViewById(R.id.password);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i1 = new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(i1);
+                if((uname.getText().toString().equals("admin") &&
+                    pword.getText().toString().equals("admin")) ||
+                        (uname.getText().toString().equals("agregory") &&
+                    pword.getText().toString().equals("waffles")) ||
+                        (uname.getText().toString().equals("nirmiter") &&
+                    pword.getText().toString().equals("pancakes"))) {
+                    Intent i1 = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(i1);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Can't log you in with what you entered", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
