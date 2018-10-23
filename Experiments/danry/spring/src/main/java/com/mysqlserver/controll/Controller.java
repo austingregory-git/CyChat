bb6b82ade8ea1ba4de17decb860416a35e774a4d
monkeyark <danryw@outlook.com>
2018-10-22 20:29:14 -0500

    add spring

---
+++ Experiments/danry/spring/src/main/java/com/mysqlserver/controll/Controller.java
@@ -0,0 +1,38 @@
+package com.mysqlserver.controll;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+
+	@Autowired
+	private Server Userver;
+
+	@RequestMapping("/user")
+	public List<UserInfor> call()
+	{
+		return Userver.findAll();
+	}
+
+	@RequestMapping("/user/{id}")
+	public Optional<UserInfor> Find(@PathVariable int id)
+	{
+		return Userver.findById(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/user")
+	public void Add(@RequestBody UserInfor user)
+	{
+		Userver.save(user);
+	}
+
+}
---
+++ Experiments/danry/spring/src/main/java/com/mysqlserver/controll/User_data.java
@@ -0,0 +1,8 @@
+package com.mysqlserver.controll;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface User_data extends CrudRepository<UserInfor, Integer>
+{
+
+}
