82e63979cb9cb48afdf916f2b5b2043bae493506
Austin Gregory <agregory@iastate.edu>
2018-12-02 22:45:26 -0600

    update schedule

---
+++ Frontend/UserInterface/UI/app/src/main/java/edu/iastate/linux/git/ui/URLConstants.java
@@ -0,0 +1,9 @@
+package edu.iastate.linux.git.ui;
+
+public class URLConstants {
+
+    public static String JSON_URL = "http://proj309-ds-01.misc.iastate.edu:8080/database";
+    public static String SOCKET_URL = "localhost:8080/add/";
+    public static String FRIEND_DISPLAY_URL = "http://proj309-ds-01.misc.iastate.edu:8080/friend/";
+}
+
